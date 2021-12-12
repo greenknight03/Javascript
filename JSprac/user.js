fe09396502820c0ebb563e0e593fdddfbd491a5c
greenknight03 <whatelse0304@gmail.com>
2021-12-13 01:39:27 +0900

    JS문법

---
+++ JSprac/user.js
@@ -0,0 +1,47 @@
+var  user = [
+  {
+    "_id": "61b6100baf324b6814588fc3",
+    "age": 34,
+    "eyeColor": "blue",
+    "name": "Jewell Smith",
+    "gender": "female",
+    "email": "jewellsmith@nutralab.com",
+    "phone": "+1 (992) 436-2922"
+  },
+  {
+    "_id": "61b6100bcd7383a7e9d73378",
+    "age": 28,
+    "eyeColor": "brown",
+    "name": "Sullivan Roach",
+    "gender": "male",
+    "email": "sullivanroach@nutralab.com",
+    "phone": "+1 (901) 521-3689"
+  },
+  {
+    "_id": "61b6100b5e56673fdb40f7be",
+    "age": 25,
+    "eyeColor": "blue",
+    "name": "Marcie Travis",
+    "gender": "female",
+    "email": "marcietravis@nutralab.com",
+    "phone": "+1 (978) 419-3105"
+  },
+  {
+    "_id": "61b6100b9b00fb57690f4267",
+    "age": 28,
+    "eyeColor": "brown",
+    "name": "Mayra Wilson",
+    "gender": "female",
+    "email": "mayrawilson@nutralab.com",
+    "phone": "+1 (926) 450-2689"
+  },
+  {
+    "_id": "61b6100be5ae7fd0abdda3f3",
+    "age": 30,
+    "eyeColor": "green",
+    "name": "Trujillo Rush",
+    "gender": "male",
+    "email": "trujillorush@nutralab.com",
+    "phone": "+1 (805) 519-3940"
+  }
+]
